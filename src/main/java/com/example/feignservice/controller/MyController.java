13b6418755085249cd45bdd7235a5ae6b1068697
Ravi Shah <64460836+ravishahtsb@users.noreply.github.com>
2021-07-20 08:23:05 +0100

    added meat to the matter

--- src/main/java/com/example/feignservice/controller/MyController.java
+++ src/main/java/com/example/feignservice/controller/MyController.java
@@ -2,7 +2,9 @@ package com.example.feignservice.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @FeignClient(value = "feign-service-v0")
 public interface MyController {
 
