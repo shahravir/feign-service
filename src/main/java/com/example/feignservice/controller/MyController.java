fb42472162dff5510a521411256313e1e67e03b8
Ravi Shah <64460836+ravishahtsb@users.noreply.github.com>
2021-07-20 11:01:04 +0100

    updated for local testing

--- src/main/java/com/example/feignservice/controller/MyController.java
+++ src/main/java/com/example/feignservice/controller/MyController.java
@@ -2,12 +2,10 @@ package com.example.feignservice.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @FeignClient(value = "feign-service-v0")
 public interface MyController {
 
     @GetMapping("/hello")
-    public String hello();
+    String hello();
 }
--- src/main/java/com/example/feignservice/controller/MyControllerImpl.java
+++ src/main/java/com/example/feignservice/controller/MyControllerImpl.java
@@ -1,10 +1,8 @@
 package com.example.feignservice.controller;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MyControllerImpl implements MyController {
 
     @Override
